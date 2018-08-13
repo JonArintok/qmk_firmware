60a1b7dc944e4225f6552b36fb970ef307880304
Jon Arintok <Jon.Arintok@Gmail.com>
2018-08-13 09:07:49 -0400

    fixed missing question mark

--- keyboards/planck/keymaps/pinkSpace/keymap.c
+++ keyboards/planck/keymaps/pinkSpace/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[lyr_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
 		{KC_VOLD,   KC_VOLU,   KC_HOME,         KC_END,          MO(lyr_num),  KC_LCTL, KC_LALT, MO(lyr_fn), KC_LGUI, KC_LEFT, KC_DOWN, KC_RGHT}
 	},
 	[lyr_num] = {
-		{KC_TILD,   KC_EXLM,   KC_LCBR,         KC_RCBR,         KC_AMPR,      KC_PIPE, KC_ASTR, KC_7,       KC_8,    KC_9,    KC_PLUS, KC_DLR},
+		{KC_TILD,   KC_EXLM,   KC_LCBR,         KC_RCBR,         KC_QUES,      KC_PIPE, KC_ASTR, KC_7,       KC_8,    KC_9,    KC_PLUS, KC_DLR},
 		{KC_COLN,   KC_SCOLON, KC_LPRN,         KC_RPRN,         KC_QUOT,      KC_AMPR, KC_SLSH, KC_4,       KC_5,    KC_6,    KC_MINS, KC_PERC},
 		{KC_BSLASH, KC_UNDS,   KC_LBRC,         KC_RBRC,         KC_DQT,       KC_HASH, KC_0,    KC_1,       KC_2,    KC_3,    KC_EQL,  KC_CIRC},
 		{XXXXXX,    XXXXXX,    KC_GRV,          KC_AT,           ______,       KC_LCTL, KC_LALT, XXXXXX,     KC_LGUI, XXXXXX,  XXXXXX,  XXXXXX}
